1c569ba23982aa02d86e3fbb178b0929d45d6f63
Jeff Montinard <68262228+jmontinard@users.noreply.github.com>
2022-12-09 11:01:43 -0500

    Update home.js

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,6 +1,6 @@
 import { Typography } from "@material-ui/core";
 
 function About() {
-  return <Typography>This is the homepage</Typography>;
+  return <Typography>This is the homepage my boy</Typography>;
 }
 export default About;

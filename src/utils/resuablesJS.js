6fb0a12c9810c49394e34631fe9108849537b173
Jeff Montinard <68262228+jmontinard@users.noreply.github.com>
2022-12-09 11:00:59 -0500

    m

---
+++ src/utils/resuablesJS.js
@@ -0,0 +1,133 @@
+export  const navItems = [
+    "Home",
+    "About",
+   "Officers",
+   "Service",
+   "Events",
+   "Contact",
+]
+
+export  const mobileNavItems = [
+    "Home",
+    "About",
+   "Officers",
+   "Service",
+   "Events",
+   "Contact",
+   "Links",
+]
+
+export  const officerPositions = [
+    "Polmarch",
+   "Vice Polemarch",
+   "Keeper of Records",
+   "Exchequer",
+   " Historian",
+   "Strategus ",
+   "Lieutenant Strategus",
+   " Grand Board  Mem 1",
+   " Grand Board  Mem 2",
+   "  Grand Board  Mem 3",
+]
+
+
+
+export  const officerPositionsCardInfo = [
+    {
+        name: "Joe Smith",
+        id:1,
+        headshots: "photos will be ref here" ,
+        position: "Polmarch",
+        officerInfo: "Lorem Ipsum  is Lorem Ipsum, Lorem Ipsum, Lorem Ipsum, Lorem Ipsum, Lorem Ipsum, Lorem Ipsum, Lorem Ipsum, Lorem Ipsum, Lorem Ipsum, Lorem Ipsum, Lorem Ipsum, Lorem Ipsum, Lorem Ipsum Ipsum",
+    },  {
+        name: "Joe Smith",
+        id:2,
+        headshots: "photos will be ref here" ,
+        position: "Vice Polemarch",
+        officerInfo: "Lorem Ipsum  is Lorem Ipsum, Lorem Ipsum, Lorem Ipsum, Lorem Ipsum, Lorem Ipsum, Lorem Ipsum, Lorem Ipsum, Lorem Ipsum, Lorem Ipsum, Lorem Ipsum, Lorem Ipsum, Lorem Ipsum, Lorem Ipsum Ipsum",
+    },
+    {
+        name: "Joe Smith",
+        id:3,
+        headshots: "photos will be ref here" ,
+        position: "Keeper of Records",
+        officerInfo: "Lorem Ipsum  is Lorem Ipsum, Lorem Ipsum, Lorem Ipsum, Lorem Ipsum, Lorem Ipsum, Lorem Ipsum, Lorem Ipsum, Lorem Ipsum, Lorem Ipsum, Lorem Ipsum, Lorem Ipsum, Lorem Ipsum, Lorem Ipsum Ipsum",
+    },  {
+        name: "Joe Smith",
+        id:4,
+        headshots: "photos will be ref here" ,
+        position: "Exchequer",
+        officerInfo: "Lorem Ipsum  is Lorem Ipsum, Lorem Ipsum, Lorem Ipsum, Lorem Ipsum, Lorem Ipsum, Lorem Ipsum, Lorem Ipsum, Lorem Ipsum, Lorem Ipsum, Lorem Ipsum, Lorem Ipsum, Lorem Ipsum, Lorem Ipsum Ipsum",
+    },
+    {
+        name: "Joe Smith",
+        id:5,
+        headshots: "photos will be ref here" ,
+        position: "Historian",
+        officerInfo: "Lorem Ipsum  is Lorem Ipsum, Lorem Ipsum, Lorem Ipsum, Lorem Ipsum, Lorem Ipsum, Lorem Ipsum, Lorem Ipsum, Lorem Ipsum, Lorem Ipsum, Lorem Ipsum, Lorem Ipsum, Lorem Ipsum, Lorem Ipsum Ipsum",
+    },  {
+        name: "Joe Smith",
+        id:6,
+        headshots: "photos will be ref here" ,
+        position: "Strategus",
+        officerInfo: "Lorem Ipsum  is Lorem Ipsum, Lorem Ipsum, Lorem Ipsum, Lorem Ipsum, Lorem Ipsum, Lorem Ipsum, Lorem Ipsum, Lorem Ipsum, Lorem Ipsum, Lorem Ipsum, Lorem Ipsum, Lorem Ipsum, Lorem Ipsum Ipsum",
+    },
+    {
+        name: "Joe Smith",
+        id:7,
+        headshots: "photos will be ref here" ,
+        position: "Lieutenant Strategus",
+        officerInfo: "Lorem Ipsum  is Lorem Ipsum, Lorem Ipsum, Lorem Ipsum, Lorem Ipsum, Lorem Ipsum, Lorem Ipsum, Lorem Ipsum, Lorem Ipsum, Lorem Ipsum, Lorem Ipsum, Lorem Ipsum, Lorem Ipsum, Lorem Ipsum Ipsum",
+    },  {
+        name: "Joe Smith",
+        id:8,
+        headshots: "photos will be ref here" ,
+        position: "Grand Board  Mem 1",
+        officerInfo: "Lorem Ipsum  is Lorem Ipsum, Lorem Ipsum, Lorem Ipsum, Lorem Ipsum, Lorem Ipsum, Lorem Ipsum, Lorem Ipsum, Lorem Ipsum, Lorem Ipsum, Lorem Ipsum, Lorem Ipsum, Lorem Ipsum, Lorem Ipsum Ipsum",
+    }, {
+        name: "Joe Smith",
+        id:9,
+        headshots: "photos will be ref here" ,
+        position: "Grand Board  Mem 2",
+        officerInfo: "Lorem Ipsum  is Lorem Ipsum, Lorem Ipsum, Lorem Ipsum, Lorem Ipsum, Lorem Ipsum, Lorem Ipsum, Lorem Ipsum, Lorem Ipsum, Lorem Ipsum, Lorem Ipsum, Lorem Ipsum, Lorem Ipsum, Lorem Ipsum Ipsum",
+    },
+    {
+        name: "Joe Smith",
+        id:10,
+        headshots: "photos will be ref here" ,
+        position: "Grand Board  Mem 3",
+        officerInfo: "Lorem Ipsum  is Lorem Ipsum, Lorem Ipsum, Lorem Ipsum, Lorem Ipsum, Lorem Ipsum, Lorem Ipsum, Lorem Ipsum, Lorem Ipsum, Lorem Ipsum, Lorem Ipsum, Lorem Ipsum, Lorem Ipsum, Lorem Ipsum Ipsum",
+    },
+
+]
+
+// export  const sliderInfo =
+
+// [
+
+//     {
+//     "id": 1,
+//     "image" : "../assets/images/sliderImg1.png",
+//     "title" : "Slider Image 1"
+    
+//     },
+//     {
+//         "id": 2,
+//         "image" : "../assets/images/sliderImg2.png",
+//         "title" : "Slider Image 2"
+        
+//         },
+//     {
+//     "id": 3,
+//     "image" : "../assets/images/sliderImg3.png",
+//     "title" : "Slider Image 3"
+    
+//     },
+//     {
+//         "id": 4,
+//         "image" : "../assets/images/sliderImg4.png",
+//         "title" : "Slider Image 4"
+        
+//         }
+    
+//     ]
---
+++ src/utils/sliderJS.js
@@ -0,0 +1,36 @@
+import sliderPic from '../assets/images/sliderImg1.png'
+import sliderPic2 from '../assets/images/sliderImg2.png'
+import sliderPic3 from '../assets/images/sliderImg3.png'
+import sliderPic4 from '../assets/images/sliderImg4.png'
+
+
+export  const sliderInfo =
+
+[
+
+    {
+    "id": 1,
+    "image" : sliderPic,
+    "title" : "Slider Image 1"
+    
+    },
+    {
+        "id": 2,
+        "image" : sliderPic2,
+        "title" : "Slider Image 2"
+        
+        },
+    {
+    "id": 3,
+    "image" : sliderPic3,
+    "title" : "Slider Image 3"
+    
+    },
+    {
+        "id": 4,
+        "image" : sliderPic4,
+        "title" : "Slider Image 4"
+        
+        }
+    
+    ]
